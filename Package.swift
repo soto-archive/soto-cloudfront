bfbba8738262c9bc488b4f17a603ae5dbe8f4f87
Adam Fowler <adamfowler71@gmail.com>
2020-03-12 18:17:35 +0000

    Version 4.2.1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
       .library(name: "CloudFront", targets: ["CloudFront"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.0.0"))
+      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
   ],
   targets: [
       .target(name: "CloudFront", dependencies: ["AWSSDKSwiftCore"]),
