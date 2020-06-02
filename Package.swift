7003a332d87597b0815c46e3fccd9994e33c5d83
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 16:49:52 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "CloudFront",
-  products: [
-      .library(name: "CloudFront", targets: ["CloudFront"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "CloudFront", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "CloudFront",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "CloudFront", targets: ["CloudFront"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "CloudFront", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/CloudFront"),
+    ]
 )
